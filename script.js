8911a310fd426080a1e11d679fc235b7531a601e
jagrutiburde0413 <124602129+jagrutiburde0413@users.noreply.github.com>
2023-04-17 18:53:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+const ul=document.getElementsByTagName("ul")[0]
+let displayText;
+for(let i=0;i<ul.children.length;i++){
+	let node=ul.children[i];
+	if(node.id=="level"){
+	displayText="The level of the element is: "+ [i+1]
+	}
+}
+alert(displayText)
